cf5b9ad455fbeb4e9c01ac364fe04acf0ce8b683
lucasrcoutinho <33065527+lucasrcoutinho@users.noreply.github.com>
2020-09-15 14:25:05 -0300

    Recebe mensagem com dados do suspeito

--- app/src/main/java/com/example/apppolicial/MainActivity.java
+++ app/src/main/java/com/example/apppolicial/MainActivity.java
@@ -15,24 +15,30 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.SQLOutput;
 
 public class MainActivity extends AppCompatActivity {
     private int STORAGE_PERMISSION_CODE = 1;
+    private BufferedReader in = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         if (!(ContextCompat.checkSelfPermission(MainActivity.this,
                 Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)){
@@ -48,11 +54,14 @@ public class MainActivity extends AppCompatActivity {
         Socket mySocket;
         Handler handler = new Handler();//Usado para acessar a thread principal
         Context context;
+        byte[] aByte = new byte[1024];
+        int bytesRead;
+        InputStream is;
+        String message;
 
         MyServer(Context c){
             context = c;
         }
-
         @Override
         public void run() {
             try {
@@ -63,27 +72,35 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(getApplicationContext(),"Waiting for client", Toast.LENGTH_SHORT).show();
                     }
                 });//Acessa thread principal para exibir mensagem flutuante
+                NotifyAlert notifyAlert = new NotifyAlert();
+
 
                 while (true){
                     System.out.println("Aguardando conexão");//Mesagem mostrada no Logcat
                     mySocket = ss.accept();
-                    InputStream is = mySocket.getInputStream();
+                    is = mySocket.getInputStream();
                     System.out.println("Conectado");
-                    NotifyAlert notifyAlert = new NotifyAlert();
                     notifyAlert.sendOnChannel1(context);
+
                     if (is!= null) {
                         FileOutputStream fos = null;
                         BufferedOutputStream bos = null;
                         try {
                             fos = new FileOutputStream("/storage/emulated/0/DCIM/Camera/000002.bmp");
                             bos = new BufferedOutputStream(fos);
-                            byte[] aByte = new byte[1024];
-                            int bytesRead;
+
+                            bytesRead = is.read(aByte);
+                            System.out.println("bytesRead lenght: " + bytesRead);
+                            String message = new String(aByte).substring(0, bytesRead);
+                            message = message.substring(0,message.indexOf("\0"));
+                            System.out.println("Bytes content: " + message);
+
 
                             while ((bytesRead = is.read(aByte)) != -1) {
                                 bos.write(aByte, 0, bytesRead);
                             }
 
+                            final String finalMessage = message;
                             handler.post(new Runnable() {
                                 @Override
                                 public void run() {
@@ -93,13 +110,17 @@ public class MainActivity extends AppCompatActivity {
                                         Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
                                         ImageView myImage = (ImageView) findViewById(R.id.imageViewT);
                                         myImage.setImageBitmap(myBitmap);
+
+
+                                        TextView textView = (TextView) findViewById(R.id.textView);
+                                        textView.setText(finalMessage);
                                     }
                                 }
                             });
                             bos.flush();
                             bos.close();
+                            mySocket.close();
                             Log.i("IMSERVICE", "FILERECCC-2");
-
                         } catch (IOException ex) {
                             // Do exception handling
                         }
