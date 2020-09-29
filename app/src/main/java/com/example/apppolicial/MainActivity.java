e5d5bf2156f536448905c1a300c5bd24b0f82502
Iago <iagojlourenco@gmail.com>
2020-09-29 00:08:19 -0300

    Git ta mt complexo

--- app/src/main/java/com/example/apppolicial/MainActivity.java
+++ app/src/main/java/com/example/apppolicial/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goToCamera(){
-        Intent intent = new Intent(this, camera.class);
+        Intent intent = new Intent(this, DetectionActivity.class);
         startActivity(intent);
     }
 
